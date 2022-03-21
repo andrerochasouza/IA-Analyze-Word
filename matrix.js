461407fd1c6adac54cec6a192d3c155bfd7ab76f
André da Rocha Souza <58944953+andrerochasouza@users.noreply.github.com>
2022-03-21 16:49:49 -0300

    new class matrix and add -> IA Analyze Word

---
+++ matrix.js
@@ -0,0 +1,38 @@
+class Matrix{
+    constructor(rows, cols){
+        this.rows = rows;
+        this.cols = cols;
+
+        this.data = [];
+
+        for (let i = 0;  i < rows; i++) {
+            let arr = []
+            for (let j = 0; j < cols; j++) {
+                arr.push(Math.floor(Math.random()*10))
+            }
+            this.data.push(arr)
+        }
+    }
+
+    map(func){
+        this.data = this.data.map((arr, i) => {
+            return arr.map((num, j) => {
+                return func(num, i, j);
+            })
+        })
+        
+        return this;
+    }
+
+    static add(A, B){
+        var matrix = new Matrix(A.rows, A.cols);
+        console.log(A.data)
+        console.log(B.data)
+        matrix.map((num, i, j) => {
+            return A.data[i][j] + B.data[i][j]
+        });
+
+        console.log(matrix.data)
+    }
+
+}
---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup(){
+    createCanvas(500, 500);
+    background(0);
+
+    var m1 = new Matrix(3, 3);
+    var m2 = new Matrix(3, 3);
+    Matrix.add(m1, m2)
+
+}
+
+function draw(){
+
+
+}
