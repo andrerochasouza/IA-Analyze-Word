637b517a7f9506448eaa7e1a11bae9de8ced4eb2
André da Rocha Souza <58944953+andrerochasouza@users.noreply.github.com>
2022-03-22 10:04:25 -0300

    test feed -> IA Analyze Word

--- matrix.js
+++ matrix.js
@@ -8,10 +8,52 @@ class Matrix{
         for (let i = 0;  i < rows; i++) {
             let arr = []
             for (let j = 0; j < cols; j++) {
-                arr.push(Math.floor(Math.random()*10))
+                arr.push(0)
             }
             this.data.push(arr)
         }
+    }
+
+    static arrayToMatrix(arr){
+        let matrix = new Matrix(arr.length, 1);
+
+        matrix.map((elm, i, j) => {
+            return arr[i];
+        })
+
+        return matrix;
+    }
+
+    static matrixToArray(obj){
+        let arr = []
+
+        obj.map((elm, i, j) => {
+            arr.push(elm);
+        })
+
+        return arr;
+    }
+
+    print(){
+        console.table(this.data);
+    }
+
+    randomize(){
+        this.map((elm, i, j) => {
+            return Math.random() * 2 - 1;
+        })
+    }
+
+    static map(A, func){
+        let matrix = new Matrix(A.rows, A.cols);
+
+        matrix.data = A.data.map((arr, i) => {
+            return arr.map((num, j) => {
+                return func(num, i, j);
+            })
+        })
+
+        return matrix;
     }
 
     map(func){
@@ -24,7 +66,39 @@ class Matrix{
         return this;
     }
 
-    static add(A, B){
+    static transpose(A){
+        var matrix = new Matrix(A.cols, A.rows);
+        matrix.map((num,i,j) => {
+            return A.data[j][i];
+        });
+        return matrix;
+    }
+
+    // Operações Estáticas Matriz x Escalar 
+    
+    static escalar_multiply(A, escalar) {
+        var matrix = new Matrix(A.rows, A.cols);
+
+        matrix.map((num, i, j) => {
+            return A.data[i][j] * escalar;
+        });
+
+        return matrix;
+    }
+    
+    // Operações Estáticas Matriz x Matriz
+
+    static hadamard(A, B) {
+        var matrix = new Matrix(A.rows, A.cols);
+
+        matrix.map((num, i, j) => {
+            return A.data[i][j] * B.data[i][j]
+        });
+
+        return matrix;
+    }
+
+    static add(A, B) {
         var matrix = new Matrix(A.rows, A.cols);
 
         matrix.map((num, i, j) => {
@@ -34,15 +108,27 @@ class Matrix{
         return matrix;
     }
 
-    static multiply(A, B){
+    static subtract(A, B) {
+        var matrix = new Matrix(A.rows, A.cols);
+
+        matrix.map((num, i, j) => {
+            return A.data[i][j] - B.data[i][j]
+        });
+
+        return matrix;
+    }
+
+    static multiply(A, B) {
         var matrix = new Matrix(A.rows, B.cols);
+
         matrix.map((num, i, j) => {
             let sum = 0
-            for(let k = 0; k < B.rows; k++){
+            for (let k = 0; k < A.cols; k++) {
                 let elm1 = A.data[i][k];
                 let elm2 = B.data[k][j];
-                sum += elm1 + elm2;
+                sum += elm1 * elm2;
             }
+            
             return sum;
         })
 
